0c5cc854ca6c587cec36f90dd5274418d58b7cbb
hubin9218 <hubin9218@163.com>
2020-03-06 10:27:28 +0800

    day01-02

---
+++ src/day02/variable1.py
@@ -0,0 +1,19 @@
+"""
+使用变量保存数据并进行操作
+
+Version: 0.1
+Author: 骆昊
+Date: 2018-02-27
+"""
+
+a=60
+b = 100
+print (a)
+print(a
+      - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+print(a**b)
